668c9ef52999ba502487b857165c9b178d34ca90
nkalupahana <avishkank@gmail.com>
2021-09-20 12:05:55 -0500

    Enable HTTP port for VM

--- main.tf
+++ main.tf
@@ -26,28 +26,17 @@ resource "google_compute_instance" "vm" {
         access_config { }
     }
 
-    metadata_startup_script = "wget https://apt.puppet.com/puppet7-release-stretch.deb; sudo dpkg -i puppet7-release-stretch.deb; sudo apt update; sudo apt install puppetserver -y; sudo systemctl start puppetserver; sudo apt install puppet-agent -y; sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true; git clone https://github.com/nkalupahana/rundeck.git; cd rundeck; puppet apply"
+    metadata_startup_script = "wget https://apt.puppet.com/puppet7-release-stretch.deb; sudo dpkg -i puppet7-release-stretch.deb; sudo apt update; sudo apt install puppetserver git -y; sudo systemctl start puppetserver; sudo apt install puppet-agent -y; sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true; puppet module install computology-packagecloud --version 0.3.2; git clone https://github.com/nkalupahana/rundeck.git; cd rundeck; sudo /opt/puppetlabs/bin/puppet apply config.pp"
 }
 
 # Allow HTTP & HTTPS
-resource "google_compute_firewall" "allow-http" {
+resource "google_compute_firewall" "allow-rundeck-http" {
     name = "${var.app_name}-fw-allow-http"
     network = "${google_compute_network.vpc.name}"
     allow {
         protocol = "tcp"
-        ports    = ["80"]
+        ports    = ["4440"]
     }
-    target_tags = ["http"]
-}
-
-resource "google_compute_firewall" "allow-https" {
-    name = "${var.app_name}-fw-allow-https"
-    network = "${google_compute_network.vpc.name}"
-    allow {
-        protocol = "tcp"
-        ports    = ["443"]
-    }
-    target_tags = ["https"]
 }
 
 resource "google_compute_firewall" "allow-ingress-from-iap" {
