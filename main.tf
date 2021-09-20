2bf8fe280369cb47753a3672b2c0a54fd6dddf7c
nkalupahana <avishkank@gmail.com>
2021-09-20 10:48:45 -0500

    Renaming & added Puppet

--- main.tf
+++ main.tf
@@ -12,50 +12,54 @@ resource "google_compute_network" "vpc" {
 
 # Create VM (with VPC)
 resource "google_compute_instance" "vm" {
-  name         = var.app_name
-  machine_type = "e2-micro"
+    name         = var.app_name
+    machine_type = "e2-micro"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-10"
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-10"
+        }
     }
-  }
 
-  network_interface {
-    # A default network is created for all GCP projects
-    network = google_compute_network.vpc.self_link
-    access_config {
+    network_interface {
+        network = google_compute_network.vpc.self_link
+        access_config { }
     }
-  }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo apt install puppet -y"
+        ]
+    }
 }
 
 # Allow HTTP & HTTPS
 resource "google_compute_firewall" "allow-http" {
-  name = "${var.app_name}-fw-allow-http"
-  network = "${google_compute_network.vpc.name}"
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-  target_tags = ["http"]
+    name = "${var.app_name}-fw-allow-http"
+    network = "${google_compute_network.vpc.name}"
+    allow {
+        protocol = "tcp"
+        ports    = ["80"]
+    }
+    target_tags = ["http"]
 }
 
 resource "google_compute_firewall" "allow-https" {
-  name = "${var.app_name}-fw-allow-https"
-  network = "${google_compute_network.vpc.name}"
-  allow {
-    protocol = "tcp"
-    ports    = ["443"]
-  }
-  target_tags = ["https"]
+    name = "${var.app_name}-fw-allow-https"
+    network = "${google_compute_network.vpc.name}"
+    allow {
+        protocol = "tcp"
+        ports    = ["443"]
+    }
+    target_tags = ["https"]
 }
 
-resource "google_compute_firewall" "allow-ingress_from_iap" {
-  name = "${var.app_name}-fw-allow-iap-ingress"
-  network = "${google_compute_network.vpc.name}"
-  source_ranges = ["35.235.240.0/20"]
-  allow {
-    protocol = "tcp"
-    ports    = ["22", "3389"]
-  }
+resource "google_compute_firewall" "allow-ingress-from-iap" {
+    name = "${var.app_name}-fw-allow-iap-ingress"
+    network = "${google_compute_network.vpc.name}"
+    source_ranges = ["35.235.240.0/20"]
+    allow {
+        protocol = "tcp"
+        ports    = ["22", "3389"]
+    }
 }
